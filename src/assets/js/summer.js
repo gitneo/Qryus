80d4c2efc6d61d0e391b9d416e565a3e21007ce5
Ogo Okafor <ogookafor@gmail.com>
2023-03-23 23:32:37 +0100

    initial commit

---
+++ src/assets/js/summer.js
@@ -0,0 +1,15 @@
+$(function () {
+  // Summernote
+  $('#summernote').summernote({
+    height: 200,                 // set editor height
+    minHeight: null,             // set minimum height of editor
+    maxHeight: null,             // set maximum height of editor
+    focus: true                  // set focus to editable area after initializing summernote
+  })
+
+  // CodeMirror
+  CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
+    mode: "htmlmixed",
+    theme: "monokai"
+  });
+})
